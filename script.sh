ea95fe1bd7969e369bd9cb97bf914921181d6064
igng <monti.d.andrea@gmail.com>
2016-09-26 10:07:53 +0200

    20-30 ok

--- script.sh
+++ script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Automated LJ_pot editing [20, 60]
-
-for i in {31..40}
+low=$1;
+upp=$((low + 10));
+for i in $(seq $low $upp)
 do
     #echo -e "\t\tRunning with N = $i"
     sed -i "s/^let N .*$/let N := $i;/g" modified_LJpot.mod
